f3b160cf1486c1f18c6383f05a1eff7ef2799e82
dosokin <dosokin@oneonmail.com>
2024-03-22 12:49:47 +0100

    update

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return (0);
     }
-    for (size_t i = 1; i < argc; i++)
+    for (size_t i = 1; i < (size_t) argc; i++)
         for (size_t j = 0; argv[i][j]; j++)
             std::cout << (char)toupper(argv[i][j]);
     std::cout << std::endl;
